7c2a41f45ed02b43701b7b28ae3667abdc0ddb0a
Edwin Duque <dea398@hotmail.com>
2019-09-03 08:59:53 -0500

    Initial COmmit

---
+++ src/ExtendedBox.cpp
@@ -0,0 +1,19 @@
+#include "ExtendedBox.h"
+
+
+void ExtendedBox::GetInfo()
+{
+
+	auto currentPos = this->getPosition();
+	this->x = currentPos.x;
+	this->y = currentPos.y;
+	this->width = this->getWidth();
+	this->height = this->getHeight();
+}
+
+void ExtendedBox::draw(int x, int y, int w, int h, int screenW, int screenH)
+{
+	GetInfo();
+	ofSetColor(this->myColor);
+	ofBoxPrimitive::draw();
+}
